a9b354a321b947121bc2af325d11b83308679f6f
Rebekka Kupiainen <rebekka.kupiainen@datanomit.local>
2018-02-23 11:00:48 +0200

    Etusivun muokkausta

--- application/views/pages/etusivu.php
+++ application/views/pages/etusivu.php
@@ -1,19 +1,17 @@
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header"><b>TIETOA</b>
-              <small><b>Palvelurakennuksen rakennus vaihe</b></small>
+              <small><b>Palvelurakennuksen rakennusvaiheista</b></small>
               <hr>
         </h1>
     </div>
 </div>
 
-<h3>Mikkelipuiston kehittämisessä ja toiminnan monipuolistamisessa otetaan iso harppaus eteenpäin uuden mallipuutarha-alueelle rakennettavan palvelurakennuksen myötä.</h3>
+<h3>Mikkelipuiston kehittämisessä ja toiminnan monipuolistamisessa otettiin iso harppaus eteenpäin uuden rakennetun Puutarharavintola Greenerin myötä.</h3>
 <br>
-<h4>Uuden rakennuksen rakentaminen on alkanut jo ja rakennus on valmiina kesäkuun 2017 lopussa, sopivasti juuri ennen asuntomessujen 2017 avautumista 14. heinäkuuta.</h4>
-<h4>Puutarhamatkailukohteen kymmenvuotisen toiminnan aikana Mikkelipuistossa on kaipailtu monipuoliseen toimintaan taipuvaa sisätilaa, joka mahdollistaa esimerkiksi erilaisten tapahtumien, näyttelyjen,
- jäsenyritysten asiakasiltojen ja muun toiminnan järjestämisen myös katon alla. Uusi palvelurakennus mahdollistaa Mikkelipuiston kehittämistä jatkossa monin tavoin pidentäen kautta ja avaten runsaasti uusia toimintamahdollisuuksia.</h4>
-<h4>Uusi puurakenteinen rakennus on mikkeliläisen Arkkitehtitoimisto Heikki Kirjalaisen suunnittelema, ja sen rakentamisesta keskeisessä roolissa ovat Etelä-Savon ammattiopiston talotekniikan opiskelijat. Rakennuksen salitila toimii niin myymälä- ja näyttelytilana kuin kahvila-ravintolana.
- Uuteen rakennukseen saadaan myös tarvittavaa henkilöstön toimistotilaa ja vuokrattavaa kokoustilaa. Rakennuttajana hankkeessa toimii Mikkelipuistoyhdistys ry. Etelä-Savon Ely-keskus on myöntänyt hankkeelle toimintaympäristön kehittämisavustusta.</h4>
-<h4>Mikkelipuisto on kesän asuntomessujen virallinen oheiskohde.</h4>
+<h4>Uuden rakennuksen rakentaminen alkoi 2016 marraskuussa ja rakennus oli valmiina ravintolan osalta kesäkuun 2017 lopussa, sopivasti juuri ennen asuntomessujen 2017 avautumista 14. heinäkuuta.</h4>
+<h4>Puutarhamatkailukohteen kymmenvuotisen toiminnan aikana Mikkelipuistossa on kaipailtu monipuoliseen toimintaan taipuvaa sisätilaa, joka mahdollistaa esimerkiksi erilaisten tapahtumien, kokousten, näyttelyjen, jäsenyritysten asiakasiltojen ja muun toiminnan järjestämisen myös katon alla. Uusi Puutarharavintola Greeneri mahdollistaa Mikkelipuiston kehittämistä jatkossa monin tavoin pidentäen kautta ympärivuotiseksi ja avaten runsaasti uusia toimintamahdollisuuksia.</h4>
+<h4>Uusi puurakenteinen rakennus on mikkeliläisen Arkkitehtitoimisto Heikki Kirjalainen Oy:n suunnittelema, ja sen rakentamisesta keskeisessä roolissa ovat Etelä-Savon ammattiopiston rakennus- ja talotekniikan opiskelijat. Rakennuksen salitila toimii niin myymälä- ja näyttelytilana kuin kahvila-ravintolana. Uuteen rakennukseen saadaan myös tarvittavaa henkilöstön toimistotilaa ja vuokrattavaa kokoustilaa. Rakennuttajana hankkeessa toimii Mikkelipuistoyhdistys ry. Etelä-Savon Ely-keskus on myöntänyt hankkeelle toimintaympäristön kehittämisavustusta.</h4>
+<h4>Mikkelipuisto oli kesän 2017 asuntomessujen virallinen oheiskohde.</h4>
 <br>
 <hr>
